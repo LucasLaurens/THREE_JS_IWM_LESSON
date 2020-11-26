dd6e83b4429cd41e53ee4c5540f74e9dc529bef4
LucasLaurens <lucaslaurens.74@gmail.com>
2020-11-26 14:49:58 +0100

    [Impr] set object class outside the main one

--- THREE_JS/main.js
+++ THREE_JS/main.js
@@ -2,6 +2,9 @@ import * as THREE from './lib/three.module.js';
 import { OrbitControls } from './lib/OrbitControls.js';
 // import Stats from './lib/stats.module.js';
 
+// Import some class
+import Objects from './objects.js';
+
 export default class Main {
 	constructor() {
 		// Set
@@ -22,7 +25,7 @@ export default class Main {
 		this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
 		// {alpha: true}
 		this.renderer = new THREE.WebGLRenderer({antialias: true});
-		this.renderer.setSize((window.innerWidth / 1.5), (window.innerHeight / 1.5));
+		this.renderer.setSize((window.innerWidth / 1.25), (window.innerHeight / 1.25));
 
 		// Cube
 		// const geometry = new THREE.BoxGeometry();
@@ -46,39 +49,8 @@ export default class Main {
 	}
 
 	initObjects() {
-		// Set all objects
-		this.boxGeometry = new THREE.BoxGeometry(.5, .5, .5);
-		this.sphereGeometry = new THREE.SphereGeometry(.3, 12, 12);
-		this.planeGeometry = new THREE.PlaneGeometry(3, 3);
-
-		// Set all materials
-		const redMaterial = new THREE.MeshBasicMaterial( {color: 0xff5555} );
-		const greenMaterial = new THREE.MeshBasicMaterial( {color: 0x55ff55} );
-		const whiteMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
-
-		// Set all mesh
-		this.boxMesh = new THREE.Mesh(this.boxGeometry, redMaterial);
-		this.sphereMesh = new THREE.Mesh(this.sphereGeometry, greenMaterial);
-		this.planeMesh = new THREE.Mesh(this.planeGeometry, whiteMaterial);
-
-		// Transform Mesh
-		this.sphereMesh.position.x = -1;
-		this.planeMesh.rotation.x = THREE.Math.degToRad(-90);
-		this.planeMesh.position.y = -1;
-		this.planeMesh.position.z = -1;
-		/**
-		 * Params :
-		 * - THREE.Math.degToRad(45);
-		 * - visible
-		 * - material (THREE.Color : Hex || Object)
-		 * - scale
-		 * - mon_mesh.clone()
-		 */
-
-		// Scene renderer
-		this.scene.add(this.boxMesh);
-		this.scene.add(this.sphereMesh);
-		this.scene.add(this.planeMesh);
+		this.objects = new Objects();
+		this.scene.add(this.objects);
 	}
 
 	onResize() {
---
+++ THREE_JS/objects.js
@@ -0,0 +1,44 @@
+import * as THREE from './lib/three.module.js';
+
+export default class Objects extends THREE.Object3D {
+	constructor() {
+		super();
+
+		// Set all objects
+		this.boxGeometry = new THREE.BoxGeometry(.5, .5, .5);
+		this.sphereGeometry = new THREE.SphereGeometry(.3, 12, 12);
+		this.planeGeometry = new THREE.PlaneGeometry(3, 3);
+
+		// Set all materials
+		const redMaterial = new THREE.MeshBasicMaterial( {color: 0xff5555} );
+		const greenMaterial = new THREE.MeshBasicMaterial( {color: 0x55ff55} );
+		const whiteMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+
+		// Set all mesh
+		this.boxMesh = new THREE.Mesh(this.boxGeometry, redMaterial);
+		this.sphereMesh = new THREE.Mesh(this.sphereGeometry, greenMaterial);
+		this.planeMesh = new THREE.Mesh(this.planeGeometry, whiteMaterial);
+
+		// Transform Mesh
+		this.sphereMesh.position.x = -1;
+		this.planeMesh.rotation.x = THREE.Math.degToRad(-90);
+		this.planeMesh.position.y = -1;
+		/**
+		 * Params :
+		 * - THREE.Math.degToRad(45);
+		 * - visible
+		 * - material (THREE.Color : Hex || Object)
+		 * - scale
+		 * - mon_mesh.clone()
+		 */
+
+		// Container
+		this.add(this.boxMesh);
+		this.add(this.sphereMesh);
+		this.add(this.planeMesh);
+		/**
+		 * Object3D est le parent
+		 * Mesh irrite de Object3D
+		 */
+	}
+}
